5a4b8a10db837aa2d38263f984967b2c59446d03
Douglas Nicholson <dnicho11@ycp.edu>
2019-03-26 00:58:56 -0400

    Finished script that takes periodic images, and GPS stamps the exif headers based on attitude

--- Camera/FinalCamera/Periodic-FlatTagged.py
+++ Camera/FinalCamera/Periodic-FlatTagged.py
@@ -1,6 +1,11 @@
 #this script takes images periodically, and modifies exif headers with dummy data.
+#Longitude = pitch, Latitude = roll, Altitude = count
+#saved as degrees, minutes and seconds are set to 0
 import time
 import serial
+import piexif
+from PIL import Image
+from picamera import PiCamera
 
 ser = serial.Serial(
         port='//dev/ttyS0',
@@ -10,16 +15,78 @@ ser = serial.Serial(
         bytesize=serial.EIGHTBITS,
         #timeout=1
 )
-active = 1  #if we want to take a picture from the safebit (data is valid)
+quality = 1 #if 1, permitted to take picture.  if 0, controller data was not accurate by sum-bit
+pitch = 0
+roll = 0
+count = 1  #used to label images
+camera = PiCamera()
 
 start = time.time()
 while 1:
     x=ser.read()
-    #print(x)
-    if x == b'$':
-        print("true")
     
+    #updates data
+    if x == b'$':
+        x=ser.read()
+        if x == b'T':
+            x=ser.read()
+            if x == b'A':
+                quality = 1 #check if quality is good, may be unnecessary
+                x=ser.read()
+                y = x
+                x=ser.read()
+                z = int.from_bytes(x, byteorder='big')
+                if z == 255:
+                    y = int.from_bytes(y, byteorder='big', signed=True)
+                    pitch = str(y)
+                else:
+                    pitch = str(y[0])
+                
+                #roll
+                x=ser.read()
+                y = x
+                x=ser.read()
+                z = int.from_bytes(x, byteorder='big')
+                if z == 255:
+                    y = int.from_bytes(y, byteorder='big', signed=True)
+                    roll = str(y)
+                else:
+                    roll = str(y[0])
+                
     end = time.time()
-    if end - start > 5 and active == 1:
+    if end - start > 5 and quality == 1:
         print(end - start)
+        #taking picture
+        imageName = 'Periodic-FlatTaggedImages//image' + str(count) + 'lat=' + roll + 'long=' + pitch + '.jpg'
+        camera.capture(imageName)
+        
+        #modifying exif GPS data
+        img = Image.open(imageName)
+        exif_dict = piexif.load(img.info['exif'])
+        
+        #exif_dict['GPS'][piexif.GPSIFD.GPSLongitude] = (int(pitch), 1)
+        #exif_dict['GPS'][piexif.GPSIFD.GPSLatitude] = (int(roll), 1)
+        exif_dict['GPS'][piexif.GPSIFD.GPSAltitude] = (count, 1)
+        exif_dict['GPS'][piexif.GPSIFD.GPSLatitude] = [((int)(round(abs(int(roll)),6) * 1000000),1000000 ),(0, 1),(0, 1)]
+        exif_dict['GPS'][piexif.GPSIFD.GPSLongitude] = [((int)(round(abs(int(pitch)),6) * 1000000),1000000 ),(0, 1),(0, 1)]
+        
+        #gps_ifd = {
+            #piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+            #piexif.GPSIFD.GPSAltitudeRef: 1,
+            #piexif.GPSIFD.GPSAltitude: count,
+            #piexif.GPSIFD.GPSLatitudeRef: "S",
+            #piexif.GPSIFD.GPSLatitude: (int(roll),0,0),
+            #piexif.GPSIFD.GPSLongitudeRef: "W",
+            #piexif.GPSIFD.GPSLongitude: (int(pitch),0,0),
+        #}
+        
+        #exif_dict = {"GPS": gps_ifd}
+        exif_bytes = piexif.dump(exif_dict)
+        piexif.insert(exif_bytes, imageName)
+        #exif_bytes = piexif.dump(exif_dict)
+        #piexif.insert(exif_bytes, imageName)
+        #img.save(imageName, "jpeg", exif=exif_bytes)
+        print("image taken")
+        count = count + 1
+        
         start = time.time()
