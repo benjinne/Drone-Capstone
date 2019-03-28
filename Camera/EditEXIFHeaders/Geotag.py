0edaffbfed5ade6786aa432f9d6796c07f746d0f
Douglas Nicholson <dnicho11@ycp.edu>
2019-03-28 09:39:47 -0400

    Updating independent scripts with new code

--- Camera/EditEXIFHeaders/Geotag.py
+++ Camera/EditEXIFHeaders/Geotag.py
@@ -12,6 +12,8 @@ print(altitude)
 #EDITING______
 
 #exif_dict['GPS'][piexif.GPSIFD.GPSLongitude] = (1400, 1)
+#exif_dict['GPS'][piexif.GPSIFD.GPSLatitude] = [((int)(round(100),6) * 1000000),1000000 ),(200, 1),(300, 1)]
+#exif_dict['GPS'][piexif.GPSIFD.GPSLongitude] = [((int)(round(400),6) * 1000000),1000000 ),(500, 1),(600, 1)]
 #exif_bytes = piexif.dump(exif_dict)
 #img.save('long1%s' % fname, "jpeg", exif=exif_bytes)
 
