90219369742a775f257b4e82146183576a2a085b
Benjamin Linne <benjinne@gmail.com>
2019-04-08 18:58:09 -0400

    updated averagePower script

--- analysis/averagePower.py
+++ analysis/averagePower.py
@@ -1,17 +1,17 @@
 import csv
 
-with open('C:/Users/Benjamin/Downloads/LOG00083.01.csv', mode='r') as csv_file:
+path = '' #enter file path here
+
+with open(path, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
     watts = 0
     for row in csv_reader:
-        volts = float(row[" vbatLatest (V)"])
-        amps  = float(row[" amperageLatest (A)"])
-        i     = int(row["loopIteration"])
-        if (i > 6860) and (i < 334130):
+        volts = float(row[" vbat"])
+        amps  = float(row[" amperage"])
+        mode  = row[" navState"]
+        if (mode == "   7"): #signifies position hold is activated
             watts = watts + (volts * amps)
             line_count = line_count + 1
-    
-    average_watts = str(watts/line_count)
+    average_watts = str((watts/line_count)/10000)
     print("average watts: " + average_watts)
-
