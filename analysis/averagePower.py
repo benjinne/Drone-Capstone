e1d02507a97df3ea5593435cde838d3a14c6a828
Benjamin Linne <benjinne@gmail.com>
2019-04-10 18:45:05 -0400

    fixed averagePower script

--- analysis/averagePower.py
+++ analysis/averagePower.py
@@ -5,13 +5,14 @@ path = '' #enter file path here
 with open(path, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
-    watts = 0
+    volts = 0
+    amps = 0
     for row in csv_reader:
-        volts = float(row[" vbat"])
-        amps  = float(row[" amperage"])
         mode  = row[" navState"]
-        if (mode == "   7"): #signifies position hold is activated
-            watts = watts + (volts * amps)
+        if (mode == "   7"): #signifies position hold is activated 
+            volts = volts + int(row[" vbat"])
+            amps = amps + int(row[" amperage"])
             line_count = line_count + 1
-    average_watts = str((watts/line_count)/10000)
-    print("average watts: " + average_watts)
+    divisor = line_count*line_count*10000
+    avgWatts = str((volts*amps)/divisor)
+    print("average watts: " + avgWatts)
